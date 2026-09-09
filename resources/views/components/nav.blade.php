2f87e6035aec71045264163a6a3f3d19c9f6107b
CitraAyu0 <meilindacitraayu@gmail.com>
2026-09-09 16:45:47 +0700

    Merapikan Navigation Link

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,5 +1,11 @@
 <nav class="bg-slate-900 text-white px-4 py-3 flex gap-4">
     <span class="font-semibold">Simple POS</span>
-    <a href="{{ route('pos.create') }}" class="hover:underline">Kasir</a>
-    <a href="{{ route('transactions.index') }}" class="hover:underline">Transaksi</a>
+    <a href="{{ route('pos.create') }}" 
+       class="hover:underline {{ request()->routeIs('pos.create') ? 'font-bold underline' : '' }}">
+        Kasir
+    </a>
+    <a href="{{ route('transactions.index') }}" 
+       class="hover:underline {{ request()->routeIs('transactions.index') ? 'font-bold underline' : '' }}">
+        Transaksi
+    </a>
 </nav>
