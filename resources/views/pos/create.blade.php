a8321ed1046fa3723b320b6d6af6643ba86359fe
N0p3l <naufalranifalutama@gmail.com>
2026-09-25 07:23:34 +0700

    tambah pagination produk di kasir

--- resources/views/pos/create.blade.php
+++ resources/views/pos/create.blade.php
@@ -42,6 +42,10 @@
             @endforeach
         </div>
 
+        <div class="mt-6">
+            {{ $products->links() }}
+        </div>
+
         <div class="mt-4 border-t pt-3">
             <template x-for="item in cart" :key="item.id">
                 <div class="flex items-center justify-between py-1">
